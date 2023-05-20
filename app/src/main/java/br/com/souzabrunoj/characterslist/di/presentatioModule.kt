b1ac521de9787679136f1a9384e809d82592551f
Bruno Souza <souza.brunoj@gmail.com>
2023-05-20 18:18:01 -0300

    Fix detekt config and added presentationModule.

--- app/src/main/java/br/com/souzabrunoj/characterslist/di/presentatioModule.kt
+++ app/src/main/java/br/com/souzabrunoj/characterslist/di/presentatioModule.kt
@@ -1,9 +1,11 @@
 package br.com.souzabrunoj.characterslist.di
 
-import br.com.souzabrunoj.characterslist.viewModel.CharactersListViewModel
+import br.com.souzabrunoj.characterslist.presentation.viewModel.CharacterDetailsViewModel
+import br.com.souzabrunoj.characterslist.presentation.viewModel.CharactersListViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val presentationModule = module {
-    viewModel { CharactersListViewModel() }
+    viewModel { CharactersListViewModel(get()) }
+    viewModel { CharacterDetailsViewModel(get()) }
 }
