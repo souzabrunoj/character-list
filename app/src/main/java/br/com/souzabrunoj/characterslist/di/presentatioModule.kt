15b68c61ae8f0b67a102c418f6e45c48228a7210
Bruno Souza <souza.brunoj@gmail.com>
2023-05-23 16:35:49 -0300

    Add inject parameter

--- app/src/main/java/br/com/souzabrunoj/characterslist/di/presentatioModule.kt
+++ app/src/main/java/br/com/souzabrunoj/characterslist/di/presentatioModule.kt
@@ -3,9 +3,10 @@ package br.com.souzabrunoj.characterslist.di
 import br.com.souzabrunoj.characterslist.presentation.viewModel.CharacterDetailsViewModel
 import br.com.souzabrunoj.characterslist.presentation.viewModel.CharactersListViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
 
 val presentationModule = module {
-    viewModel { CharactersListViewModel(get()) }
+    viewModel { (name: String, status: String) -> CharactersListViewModel(get { parametersOf(name, status) }) }
     viewModel { CharacterDetailsViewModel(get()) }
 }
